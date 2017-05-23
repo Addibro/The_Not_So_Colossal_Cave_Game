fba8021251e25c444b2a60dd60c64eb551c7057d
andreas <andreas.alme.eriksson@gmail.com>
2017-05-23 15:49:09 +0200

    add java doc to apply

--- src/se/itu/game/cave/interfaces/ThingRule.java
+++ src/se/itu/game/cave/interfaces/ThingRule.java
@@ -3,5 +3,11 @@ package se.itu.game.cave.interfaces;
 import se.itu.game.cave.exceptions.RuleViolationException;
 
 public interface ThingRule {
+    /**
+     * Returns true if no rule if present to the given thing,
+     * otherwise it will throw an exception
+     * @return true or throws an exception
+     * @throws RuleViolationException if the condition(s) of the rule isn't met
+     */
     boolean apply() throws RuleViolationException;
 }
