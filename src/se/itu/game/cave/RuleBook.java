d8a5416c9067c3ec45cc09c241f2724410c546c5
andreas <andreas.alme.eriksson@gmail.com>
2017-05-23 15:50:16 +0200

    add java docs to all methods

--- src/se/itu/game/cave/RuleBook.java
+++ src/se/itu/game/cave/RuleBook.java
@@ -30,6 +30,14 @@ public class RuleBook {
     private static Map<Thing, ThingRule> rules = new HashMap<>();
     private static Map<Room, RoomRule> roomRules = new HashMap<>();
 
+    /**
+     * Maps rules to certain Things. The rule is an interface
+     * which can be applied anonymously.
+     * @param thing the object which the rule is to be applied to
+     * @param rule the thing rule which is to be applied
+     * @see ThingRule
+     * @see RuleBook
+     */
     public static void addThingRule(Thing thing, ThingRule rule) {
         rules.put(thing, rule); // The rule is of ThingRule type
                                 // i.e an interface and will implement
@@ -37,10 +45,25 @@ public class RuleBook {
                                 // (either true or throw new RuleViolationException)
     }
 
+    /**
+     * Maps rules to certain Rooms. The rule is abstract, so can be
+     * applied anonymously.
+     * @param room the room object which the rule is to be applied to
+     * @param roomRule the room rule which is to be applied
+     * @see RoomRule
+     * @see RuleBook
+     */
     public static void addRoomRule(Room room, RoomRule roomRule) {
         roomRules.put(room, roomRule);
     }
 
+    /**
+     * Returns a rule for a given thing. If no rule is found
+     * it returns a rule set to true.
+     * @param thing the thing to checked for rules
+     * @return the rule for the given thing
+     * @see ThingRule
+     */
     public static ThingRule getRuleFor(Thing thing) {
         ThingRule rule = rules.get(thing); // Give us the rule for the Thing (if any)
         if (rule == null) {     // If the Thing didn't have a ThingRule (null)
@@ -49,6 +72,14 @@ public class RuleBook {
         return rule;
     }
 
+    /**
+     * Returns a rule for a given room. If no rule is found
+     * it returns an "empty" rule, meaning it doesn't contain
+     * any room or description.
+     * @param room the room to be checked for rules
+     * @return the room rule for the given room
+     * @see RoomRule
+     */
     public static RoomRule getRuleFor(Room room) {
         RoomRule roomRule = roomRules.get(room);
         if (roomRule == null) {
