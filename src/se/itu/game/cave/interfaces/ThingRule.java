bab092a7c5e2db961182c83321f5318ad5b53977
andreas <andreas.alme.eriksson@gmail.com>
2017-05-25 10:10:27 +0200

    add java doc comments

--- src/se/itu/game/cave/interfaces/ThingRule.java
+++ src/se/itu/game/cave/interfaces/ThingRule.java
@@ -2,6 +2,12 @@ package se.itu.game.cave.interfaces;
 
 import se.itu.game.cave.exceptions.RuleViolationException;
 
+/**
+ * An abstract idea of a rule for a Thing in the game.
+ * Will be defined in initiation of the game and executed
+ * during the game play
+ * @see se.itu.game.cave.RuleBook
+ */
 public interface ThingRule {
     /**
      * Returns true if no rule if present to the given thing,
