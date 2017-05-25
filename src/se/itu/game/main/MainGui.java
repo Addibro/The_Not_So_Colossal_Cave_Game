8ebf5a251b0afe63dbc2d2df82aa990114b150ab
andreas <andreas.alme.eriksson@gmail.com>
2017-05-25 10:08:13 +0200

    add java docs comments and call to run() in main menu

--- src/se/itu/game/main/MainGui.java
+++ src/se/itu/game/main/MainGui.java
@@ -1,12 +1,14 @@
 package se.itu.game.main;
 
-import se.itu.game.gui.MainFrame;
+import se.itu.game.gui.MainMenu;
 
+/**
+ * Class with main method. This is the
+ * starting point of the game.
+ */
 public class MainGui {
 
   public static void main(String[] args) {
-    MainFrame mainFrame = new MainFrame();
-    mainFrame.run();
+      MainMenu.run();
   }
-  
 }
