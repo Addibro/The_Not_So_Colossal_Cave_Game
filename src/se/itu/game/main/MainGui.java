73356ee1b7c1e7eb74ea5d3b6ff42037643171c7
andreas <andreas.alme.eriksson@gmail.com>
2017-05-25 10:56:12 +0200

    add java doc comment

--- src/se/itu/game/main/MainGui.java
+++ src/se/itu/game/main/MainGui.java
@@ -7,7 +7,12 @@ import se.itu.game.gui.MainMenu;
  * starting point of the game.
  */
 public class MainGui {
-
+    /**
+     * This is the starting point of the game, calling
+     * the run method in MainMenu
+     * @param args the args
+     *             @see MainMenu
+     */
   public static void main(String[] args) {
       MainMenu.run();
   }
